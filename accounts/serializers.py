51d550a6a8afe1038cdaa9418e4fe153e086f66a
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-29 12:50:42 -0300

    add to_representation for user inside PostSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,7 +10,6 @@ class UserSerializer(AbstractSerializer):
         model = User
         # List of all the fields that can be included in a request or a response
         fields = [
-            # "id",
             "username",
             "name",
             "first_name",
@@ -19,9 +18,7 @@ class UserSerializer(AbstractSerializer):
             "avatar",
             "email",
             "is_active",
-            # "created",
-            # "updated",
-            "posts_count",
+            # "posts_count",
         ]
         # List of all the fields that can only be read by the user
         read_only_field = ["is_active"]
