93003f4333ad7ea822827d4752f708e45150dc51
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-29 10:58:58 -0300

    add auth-refresh, 50

--- auth/urls.py
+++ auth/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from auth.views import LoginViewSet, RegisterViewSet
+from auth.views import LoginViewSet, RefreshViewSet, RegisterViewSet
 
 router = routers.SimpleRouter()
 router.register(r"auth/register", RegisterViewSet, basename="auth-register")
 router.register(r"auth/login", LoginViewSet, basename="auth-login")
+router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
 
 urlpatterns = [
     path("", include(router.urls)),
--- auth/views/refresh.py
+++ auth/views/refresh.py
@@ -1,19 +1,17 @@
-from rest_framework import status
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
-from auth.serializers import LoginSerializer
 
-
-class LoginViewSet(ViewSet):
-    serializer_class = LoginSerializer
+class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
     permission_classes = (AllowAny,)
     http_method_names = ["post"]
 
     def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.get_serializer(data=request.data)
 
         try:
             serializer.is_valid(raise_exception=True)
