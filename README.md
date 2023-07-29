fc6f337dd0bc6ca0a1d18a6e3c46bc6656a3381f
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-28 21:48:13 -0300

    add accounts app

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Run PostgreSQL DB in Docker:
 ```
 docker run -d --name postgres_blogs_container -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
 ```
+
+Superuser:
+```
+Email: root@site.com
+Username: root
+Password: 1
+```
--- project/settings.py
+++ project/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # internal apps
+    "accounts",
+    # external apps
 ]
 
 MIDDLEWARE = [
@@ -137,3 +140,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
