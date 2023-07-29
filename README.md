c4007889724c9f5ae3bb714a631524ace1c163b2
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-28 21:54:12 -0300

    pre-commit

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ pre-commit run --all-files
 
 Run PostgreSQL DB in Docker:
 ```
-docker run -d --name postgres_blogs_container -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 postgres
+docker run -d --name postgres_blogs_container -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -v postgres_blogs_data:/var/lib/postgresql/data postgres
 ```
 
 Superuser:
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 import uuid
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.http import Http404
